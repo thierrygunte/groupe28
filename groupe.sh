201ce27293f43ad2e753341cb030da23e3aeac6d
thierrygunte <67665010+thierrygunte@users.noreply.github.com>
2022-01-11 20:44:33 +0100

    Add files via upload

---
+++ groupe.sh
@@ -0,0 +1,20 @@
+
+echo "bienvenu a l'iut Fotso Victor de Bandjoun "
+
+echo " mise a jour du systeme  "
+sudo  apt-get update
+echo "  Installation  "
+
+sudo apt-get install gcc
+sudo apt-get install gedit
+sudo apt-get install git
+
+echo "  ENVIRONNEMENT DEPLOYER  "
+
+https://github.com/thierrygunte/groupe28.git
+ls -l
+cd groupe10
+gcc -Wall main.c -o main -lm
+chmod +x main.c
+./main
+
